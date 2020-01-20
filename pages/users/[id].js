44a75abf23c633b916208fafe31c51bf13d85db6
Natalya <nata.basovska@gmail.com>
2020-01-20 14:31:13 -0500

    Add base structure to user profile page-3

--- pages/users/[id].js
+++ pages/users/[id].js
@@ -1,5 +1,9 @@
 import React from 'react';
 import Head from 'next/head';
+import Layout from 'shared/components/layout';
+import { connect } from 'react-redux';
+import { withUser } from 'shared/components/withUser';
+import { withError } from 'shared/components/withError';
 
 function UserProfile () {
   return (
@@ -31,6 +35,7 @@ const mapDispatchToProps = {};
 
 export default connect(null, mapDispatchToProps)(
   withError(
-    
+    withUser(UserProfile),
+    'admin'
   )
 );
