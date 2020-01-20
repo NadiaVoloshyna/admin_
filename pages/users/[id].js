ebac2d10b82bdda44d85817e535f82f1902996ed
Natalya <nata.basovska@gmail.com>
2020-01-20 10:38:37 -0500

    Add base structure to user profile page

--- pages/users/[id].js
+++ pages/users/[id].js
@@ -1,14 +1,22 @@
 import React from 'react';
 import Head from 'next/head';
 
-function Users () {
+function UserProfile () {
   return (
     <div className="users-page">
       <Head>
         <title>Users</title>
         <link rel='icon' href='/favicon.ico' />
       </Head>
-      
+
+      <Layout activePage="UserProfile" >
+        <Layout.Navbar>
+        </Layout.Navbar>
+
+        <Layout.Content>
+        </Layout.Content>
+      </Layout>
+
       <div>
         <div>Useres List</div>
       </div>
@@ -16,4 +24,19 @@ function Users () {
   )
 };
 
-export default Users;
+Users.getInitialProps = ({ a }) => {
+  const { store } = a ;
+
+  // Set initial state
+  store.dispatch(pageActions.usersInitialState(initialState));
+  store.dispatch(pageActions.getUsers());
+}
+
+const mapDispatchToProps = {};
+
+export default connect(null, mapDispatchToProps)(
+  withError(
+    withUser(UserProfile),
+    'admin'
+  )
+);
