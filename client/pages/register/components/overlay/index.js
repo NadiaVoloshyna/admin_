de0cfab9fc7dee1ae0636d99f67d90c12e1fcb02
Oleksandr Kukoba <alekskukoba@gmail.com>
2020-05-27 22:12:30 +0300

    Split auth pages into top level pages and client pages

---
+++ client/pages/register/components/overlay/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import Popover from 'react-bootstrap/Popover';
+
+const Overlay = (props) => {
+  return (
+    <Popover>
+      <Popover.Content>
+        Valid password must be
+        <ul>
+          <li>at least 8 characters long</li>
+          <li>at most 18 characters long</li>
+        </ul>
+        Must have
+        <ul>
+          <li>at least one upper case letter</li>
+          <li>at least one lower case letter</li>
+          <li>at least one integer</li>
+        </ul>
+        Must contain
+        <ul>
+          <li>Must contain at least one of special characters</li>
+        </ul>
+        Allowed special characters are:
+        {/* TODO: move to constants */}
+        <p><b>@ % + ' ! # $ & * ^ ? : . ( ) { } ~ - _ .</b></p>
+      </Popover.Content>
+    </Popover>
+  );
+}
+
+export default Overlay;
