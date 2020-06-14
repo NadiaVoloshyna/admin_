78ccefcf130e1b9b22d43518e122629ba4b8e2f2
uc2pac <uc2pac@gmail.com>
2020-06-14 10:12:28 -0400

    Added change role for signed in user functionality

--- next.config.js
+++ next.config.js
@@ -14,6 +14,7 @@ module.exports = withCSS(withSass({
       'assets': path.join(__dirname, 'assets'),
       'utils': path.join(__dirname, 'client/utils'),
       'store': path.join(__dirname, 'client/store'),
+      'common': path.join(__dirname, 'common'),
     };
     return config;
   },
