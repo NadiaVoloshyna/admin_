e42103f18aa2c65b84eda7d95ceb6183ceeaab40
uc2pac <uc2pac@gmail.com>
2020-07-01 13:15:58 -0400

    Update assets.[id].delete.js

--- server/routes/assets/assets.[id].delete.js
+++ server/routes/assets/assets.[id].delete.js
@@ -7,14 +7,13 @@ const ac = require('../../../accesscontrol.config');
 const checkPermissions = (req, res, next) => {
   const { user } = req;
   const canDelete = ac.can(user.role).deleteAny('assets').granted;
-
+  
   if (!canDelete) return res.status(403).end();
 
   next();
+};
 
-}
-
-const deleteAssets = async (req, res, next) => {
+const deleteAssets = async (req, res) => {
   const { id } = req.params;
 
   try {
@@ -28,19 +27,20 @@ const deleteAssets = async (req, res, next) => {
   } catch (error) {
     return handleError.custom(res, 500, error);
   }
-}
+};
 
 module.exports = (router) => {
   /**
- * Delete one or multiple assets
- * 1. Delete asset
- *   a. if user is super admin delete assets
- *   b. otherwise mark as deleted
- * 2. Delete cloudinary asset
- */
+  * Delete one or multiple assets
+  * 1. Delete asset
+  *   a. if user is super admin delete assets
+  *   b. otherwise mark as deleted
+  * 2. Delete cloudinary asset
+  */
   router.delete('/:id', [
     check('id').exists().isMongoId(),
-  ], errorHandler,
-     checkPermissions,
-     deleteAssets);
-}
+  ], 
+  errorHandler,
+  checkPermissions,
+  deleteAssets);
+};
