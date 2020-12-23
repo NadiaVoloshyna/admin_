2408a8c042d3c53003e6c4d78ced95bb8e6e7ab6
mdresvyankin <mdresvyankin@gmail.com>
2020-12-23 14:35:48 +0200

    Fix after review

--- server/routes/users/users.[id].get.js
+++ server/routes/users/users.[id].get.js
@@ -1,15 +1,13 @@
-const { body, param } = require('express-validator');
+const { param } = require('express-validator');
 const User = require('../../models/user');
 const handle400 = require('../../middlewares/errorHandlers/handle400');
 
 module.exports = (router) => {
   router.get('/:id', [
-    param('id').isMongoId(),
-    body('active').if(body('active').exists()).isBoolean()
+    param('id').isMongoId().isBoolean()
   ], handle400, async (req, res) => {
-    const user = await User.findById({ _id: req.params.id });
-
     try {
+      const user = await User.findById({ _id: req.params.id });
       res.status(200).send(user);
     } catch (error) {
       req.handle500(error);
