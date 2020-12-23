dc790d0607a80928776d1e00b47e0908815e0f1c
mdresvyankin <mdresvyankin@gmail.com>
2020-12-23 14:27:04 +0200

    Simplify changes

--- client/shared/components/searchField/index.js
+++ client/shared/components/searchField/index.js
@@ -1,41 +1,32 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import _debounce from 'lodash/debounce';
 import Form from 'react-bootstrap/Form';
 import useListDataFetch from 'shared/hooks/useListDataFetch';
 
 const SearchField = () => {
   const { addQueryParams, removeQueryParam, getQueryParams } = useListDataFetch();
+  const searchTerm = getQueryParams('q');
 
-  const initState = {
-    searchTerm: getQueryParams('q'),
-    reload: false,
-  };
-  const [state, setReload] = useState(initState);
-  useEffect(() => {
-    if (state.reload) {
-      if (!state.searchTerm || state.searchTerm === '') {
-        removeQueryParam('q');
-      }
-      addQueryParams('q', state.searchTerm);
-    }
-    return () => {};
-  }, [state]);
+  // Debouncing getting new users set after search
+  const debouncedSearch = _debounce((searchTerm) => {
+    if (!searchTerm) return removeQueryParam('q');
+    addQueryParams('q', searchTerm);
+  }, 700);
 
-  const callApi = (evt) => {
-    setReload({
-      ...state,
-      searchTerm: evt.searchTerm,
-      reload: true,
-    });
-  };
+  /**
+   * Sets search term
+   * @param {Object} event syntetic event
+   */
+  const onSearchChange = (event) => debouncedSearch(event.target.value);
 
-  const [debouncedCallApi] = useState(() => _debounce(callApi, 700));
-
-  function handleChange(evt) {
-    debouncedCallApi({ searchTerm: evt.target.value });
-  }
-
-  return <Form.Control type="search" placeholder="Search" onChange={handleChange} defaultValue={state.searchTerm} />;
+  return (
+    <Form.Control
+      type="search"
+      placeholder="Search"
+      onChange={onSearchChange}
+      defaultValue={searchTerm}
+    />
+  );
 };
 
 export default SearchField;
