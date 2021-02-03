08b9f5ea377804256b8b2a719a6b33923a93c654
NadiaVoloshyna <dnipronadiya@gmail.com>
2021-02-03 15:55:50 +0200

    added logging handle400

--- server/middlewares/errorHandlers/handle400.js
+++ server/middlewares/errorHandlers/handle400.js
@@ -1,5 +1,20 @@
 const { validationResult } = require('express-validator');
 const createResponseBody = require('./createResponseBody');
+const { logger } = require('../../services/gcp/logger');
+const { HTTP_HEADERS, LOG_TYPE } = require('../../../common/constants');
+
+const createErrorPayload = (req, err) => {
+  const { message, stack, code, status } = err;
+  return {
+    traceId: req.get(HTTP_HEADERS.X_TRACE_ID),
+    status,
+    code,
+    message,
+    errorType: LOG_TYPE.API,
+    stack,
+    url: req.originalUrl,
+  };
+};
 
 const handle400 = (req, res, next) => {
   const errors = validationResult(req).formatWith(({ location, msg, param }) => {
@@ -9,6 +24,8 @@ const handle400 = (req, res, next) => {
   // TODO: log errors
 
   if (errors.array().length) {
+    const err = new Error('badRequest');
+    logger.error(createErrorPayload(req, err));
     const responseBody = createResponseBody(errors.array(), 'badRequest');
     return res.status(400).send(responseBody);
   }
